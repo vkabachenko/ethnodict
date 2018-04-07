00273fcba4d237bf92d50ba546567ff2a48313a4
azizMavlyanov <Alex.mavlyanov95@gmail.com>
2018-04-07 11:41:25 +0300

    Add dependencies like normalize.css and font-awesome to package.json and main.js

--- frontend/web/src/main.js
+++ frontend/web/src/main.js
@@ -3,6 +3,8 @@ import App from './App'
 import router from './router'
 import store from './store'
 import 'bootstrap/dist/css/bootstrap.css'
+import 'normalize.css/normalize.css'
+import 'font-awesome/css/font-awesome.css'
 
 Vue.config.productionTip = false
 
