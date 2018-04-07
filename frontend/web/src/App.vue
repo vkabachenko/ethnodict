1c0775b2521daf13eb8091300dbc8de81f80d094
azizMavlyanov <Alex.mavlyanov95@gmail.com>
2018-04-07 10:47:08 +0300

    Create HTML code for header section and footer section

--- frontend/web/src/App.vue
+++ frontend/web/src/App.vue
@@ -1,17 +1,53 @@
 <template>
   <div id="app">
-    <div class="container">
-      <h1> Header</h1>
-    </div>
+    <header class="header">
+      <div class="container">
+        <div class="logo">
+          <img src="images/logo.png" alt="" class="logo__img">
+        </div>
+        <nav class="nav">
+          <ul class="nav__list">
+            <li class="nav__item">
+              <a href="index.html" class="nav__link">Главная</a>
+            </li>
+            <li class="nav__item">
+              <a href="listofwords.html" class="nav__link">Список слов</a>
+            </li>
+            <li class="nav__item">
+              <a href="contacts.html" class="nav__link">Контакты</a>
+            </li>
+          </ul>
+        </nav>
+        <div class="contacts">
+          <ul class="contacts__list">
+            <li class="contacts__item"><i class="fa fa-map-marker" aria-hidden="true"></i> <span class="add">Адрес</span></li>
+            <li class="contacts__item"><i class="fa fa-phone" aria-hidden="true"></i> <span>Телефон</span></li>
+            <li class="contacts__item"><i class="fa fa-envelope" aria-hidden="true"></i> <span>Почта</span></li>
+          </ul>
+        </div>
+      </div>
+    </header>
 
     <div class="container">
       <router-view/>
     </div>
-
-    <div class="container">
-      <footer>Footer</footer>
-    </div>
-
+    <footer>
+      <div class="container">
+        <div class="logo">
+          <img src="images/logo.png" alt="" class="logo__img">
+        </div>
+        <div class="pskovgu-logo">
+          <img src="images/pskgu-icon.png" alt="" class="pskovgu-logo__img">
+        </div>
+        <div class="contacts footer__contacts">
+          <ul class="contacts__list">
+            <li class="contacts__item"><i class="fa fa-map-marker" aria-hidden="true"></i> <span class="add">Адрес</span></li>
+            <li class="contacts__item"><i class="fa fa-phone" aria-hidden="true"></i> <span>Телефон</span></li>
+            <li class="contacts__item"><i class="fa fa-envelope" aria-hidden="true"></i> <span>Почта</span></li>
+          </ul>
+        </div>
+      </div>
+    </footer>
   </div>
 </template>
 
