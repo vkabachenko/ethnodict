5b4cafe09222e0f88a5583f2ab841d35f29d5c47
Viktor Kabachenko <vkabachenko@gmail.com>
2018-06-11 15:19:35 +0300

    Add Yandex Metrica to frontend

--- frontend/web/src/main.js
+++ frontend/web/src/main.js
@@ -2,11 +2,17 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import store from './store'
+import VueYandexMetrika from 'vue-yandex-metrika'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'normalize.css/normalize.css'
 import 'font-awesome/css/font-awesome.css'
 
 Vue.config.productionTip = false
+Vue.use(VueYandexMetrika, {
+  id: process.env.YANDEX_METRICA_ID,
+  router: router,
+  env: process.env.NODE_ENV
+})
 
 /* eslint-disable no-new */
 new Vue({
