2c81ad8a2b41a83f75031cf82c97eade4c124c08
Viktor Kabachenko <vkabachenko@gmail.com>
2018-06-12 10:50:05 +0300

    Template for frontend environment variables

--- frontend/web/config/prod.env.js
+++ frontend/web/config/prod.env.js
@@ -1,4 +1,7 @@
 'use strict'
 module.exports = {
-  NODE_ENV: '"production"'
+  NODE_ENV: '"production"',
+  API_URL: '"http://api.url/"',
+  RECAPTCHA_SITE_KEY: '"Site_key_from_Google"',
+  YANDEX_METRICA_ID: '00000000'
 }
